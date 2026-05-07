cbade7231e24cf2cc4f2ecc98b46e2495e682f84
Donjaex <150985708+Donjaex@users.noreply.github.com>
2026-05-07 18:35:24 -0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -52,7 +52,7 @@ def build_keywords_for_docs(docs, top_n=25):
 DB_CONFIG = {
     "host": "127.0.0.1",
     "user": "root",
-    "password": "",
+    "password": "Happy321",
     "database": "youthsmart",
     "port": 3306,
 }
